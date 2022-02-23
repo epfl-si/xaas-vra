78fb860018de6805d0c6bf33382926a955ac06b1
Chaboudez Lucien <lucien.chaboudez@epfl.ch>
2022-02-23 10:40:56 +0100

    Add ssh configuration

--- CloudInit/ubuntu/cloudinit.sh
+++ CloudInit/ubuntu/cloudinit.sh
@@ -1,3 +1,14 @@
 #!/bin/bash
 
 apt-get update
+
+# SSH configuration
+echo "SSH Configuration"
+SSHD_FILE=/etc/ssh/sshd_config
+grep -v "PermitRootLogin" ${SSHD_FILE} > ${SSHD_FILE}
+echo "PermitRootLogin yes" >> ${SSHD_FILE}
+
+grep -v "PasswordAuthentication" ${SSHD_FILE} > ${SSHD_FILE}
+echo "PasswordAuthentication yes" >> ${SSHD_FILE}
+
+service ssh restart
