2a53eaa51984d8355402cb4cb605c6fc2979948c
maciek <Maciej.Macowicz@epfl.ch>
2022-03-19 11:50:43 +0100

    [NAS.New] Utility to copy JSON files to IDE: added default param values

--- cp_form.sh
+++ cp_form.sh
@@ -1,8 +1,9 @@
 #
-CATEGORY='Forms/NAS (Public)/Day 2'
-[ -z "$1" ] && echo "Give form name" && exit 255
-echo "going with Forms/NAS (Public)/Day 2/$1"
-[ -f "$HOME/Downloads/$1.json" ] && echo "JSON" && python -m json.tool $HOME/Downloads/$1.json > "$CATEGORY/$1.json"
-rm -f $HOME/Downloads/$1.json
-[ -f "$HOME/Downloads/$1.css" ] && echo "CSS" && cp $HOME/Downloads/$1.css "$CATEGORY/$1.css"
-rm -f $HOME/Downloads/$1.css
+CATEGORY='Forms/NAS (Public)/Day 1'
+FORM_NAME="NAS Volume"
+[ -z "$FORM_NAME" ] && echo "Give form name" && exit 255
+echo "going with Forms/NAS (Public)/Day 2/${FORM_NAME}"
+[ -f "$HOME/Downloads/${FORM_NAME}.json" ] && echo "JSON" && python -m json.tool "$HOME/Downloads/${FORM_NAME}.json" > "$CATEGORY/${FORM_NAME}.json"
+rm -f $HOME/Downloads/${FORM_NAME}.json
+[ -f "$HOME/Downloads/${FORM_NAME}.css" ] && echo "CSS" && cp $HOME/Downloads/${FORM_NAME}.css "$CATEGORY/${FORM_NAME}.css"
+rm -f $HOME/Downloads/${FORM_NAME}.css
