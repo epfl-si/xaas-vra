a98135bf9d7b62270d25357dd902198541b9377b
maciek <Maciej.Macowicz@epfl.ch>
2022-03-23 19:36:32 +0100

    [NAS.Fix] cp_forms.sh: no python on macos 12.3. Use brew-installed python3 instead.

--- cp_form.sh
+++ cp_form.sh
@@ -19,14 +19,14 @@ GIT_JSON=$CATEGORY/$FORM_NAME
   exit 255
 
 /bin/echo -n " * Comparing $GIT_JSON with download: "
-python -m json.tool "$DOWNLOAD" | diff -w - "$GIT_JSON" 2> /dev/null
+python3 -m json.tool "$DOWNLOAD" | diff -w - "$GIT_JSON" 2> /dev/null
 DIFFS=$?
 [ $DIFFS -eq 0 ] &&\
   echo "no differences found" &&\
   rm -f "$DOWNLOAD" &&\
   exit 0;
 
-python -m json.tool "$DOWNLOAD" > "$GIT_JSON"
+python3 -m json.tool "$DOWNLOAD" > "$GIT_JSON"
 echo " * Copied to $GIT_JSON"
 echo " * git commit !!!"
 rm -f "$DOWNLOAD"
