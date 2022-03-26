582e4f4392dedb22d365f342f4b720660bc0022f
maciek <Maciej.Macowicz@epfl.ch>
2022-03-26 14:26:54 +0100

    [NAS] cp_form.sh refactor

--- cp_form.sh
+++ cp_form.sh
@@ -8,6 +8,27 @@ CATEGORY="Forms/NAS (Public)/Day ${2:-2}"
 DOWNLOAD=$HOME/Downloads/$FORM_NAME
 GIT_JSON=$CATEGORY/$FORM_NAME
 
+compare() {
+  /bin/echo -n " * Comparing $GIT_JSON with download: "
+  # python3 -m json.tool "$DOWNLOAD" | diff -w - "$GIT_JSON" 2> /dev/null
+  jq < "$DOWNLOAD" | diff -w - "$GIT_JSON" > /dev/null 2>&1
+  DIFFS=$?
+  if [ $DIFFS -eq 0 ]; then
+    echo "no differences found"
+    rm -f "$DOWNLOAD"
+    exit 0;
+  else
+    echo "different"
+  fi
+}
+
+copy() {
+  # python3 -m json.tool "$DOWNLOAD" > "$GIT_JSON"
+  jq < "$DOWNLOAD" > "$GIT_JSON"
+  echo " * Copied to $GIT_JSON"
+  rm -f "$DOWNLOAD"
+}
+
 [ -z "$FORM_NAME" ] && echo "No form name given" && exit 255
 
 [ ! -f "$DOWNLOAD" ] &&\
@@ -16,19 +37,10 @@ GIT_JSON=$CATEGORY/$FORM_NAME
 
 [ ! -f "$GIT_JSON" ] &&\
   echo "$FORM_NAME not found in project ${CATEGORY}" &&\
-  exit 255
+  copy &&\
+  echo " * git add; git commit" &&\
+  exit 0
 
-/bin/echo -n " * Comparing $GIT_JSON with download: "
-# python3 -m json.tool "$DOWNLOAD" | diff -w - "$GIT_JSON" 2> /dev/null
-jq < "$DOWNLOAD" | diff -w - "$GIT_JSON" 2> /dev/null
-DIFFS=$?
-[ $DIFFS -eq 0 ] &&\
-  echo "no differences found" &&\
-  rm -f "$DOWNLOAD" &&\
-  exit 0;
-
-# python3 -m json.tool "$DOWNLOAD" > "$GIT_JSON"
-jq < "$DOWNLOAD" > "$GIT_JSON"
-echo " * Copied to $GIT_JSON"
-echo " * git commit !!!"
-rm -f "$DOWNLOAD"
+compare
+copy
+echo " * git commit"
