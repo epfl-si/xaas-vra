0c659eced46c7f31857a2323460bc73c10e4c4af
Chaboudez Lucien <lucien.chaboudez@epfl.ch>
2022-07-06 11:29:13 +0200

    Change file locations

--- CloudInit/base.sh
+++ CloudInit/base.sh
@@ -1,13 +1,19 @@
 #!/bin/bash
 
 # Constantes
-localFuncScript="/tmp/func.sh"
-localScript="/tmp/cloudinit.sh"
+localFuncScript="/root/cloud-init/func.sh"
+localScript="/root/cloud-init/cloudinit.sh"
 debugFile="/root/cloud-init/cloud-init.debug"
 
+# Si on n'est pas en mode debug
+if [ -f ${debugFile} ]
+then
+    set -x
+fi
+
 # Attente que l'application du tag dans NSX soit bien effective
-echo "Sleeping until network is available in NSX..."
-sleep 30
+#echo "Sleeping until network is available in NSX..."
+#sleep 30
 
 # Récupération des infos
 gitBranch=`cat /root/cloud-init/_repo-branch`
@@ -17,11 +23,7 @@ cloudInitBaseImage=`cat /root/cloud-init/_base-image`
 wget https://raw.githubusercontent.com/epfl-si/xaas-vra/${gitBranch}/CloudInit/${cloudInitBaseImage}/cloudinit.sh -O ${localScript}
 wget https://raw.githubusercontent.com/epfl-si/xaas-vra/${gitBranch}/CloudInit/func.sh -O ${localFuncScript}
 
-# Si on n'est pas en mode debug
-if [ -f ${debugFile} ]
-then
-    set -x
-fi
+
 # Exécution
 echo "Executing script..."
 chmod u+x ${localScript}
