d482c9e2bf41923bb39742c0fcd29cbd93478ec7
Chaboudez Lucien <lucien.chaboudez@epfl.ch>
2022-07-07 12:08:50 +0200

    System upgrade, enable SSH

--- CloudInit/ubuntu/cloudinit.sh
+++ CloudInit/ubuntu/cloudinit.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
 
+# Mise à jour des packages
 apt-get update
+apt-get upgrade
 
-testLC
+# Activation du SSH
+echo "Activating SSH..."
+sed -i 's/#PermitRootLogin prohibit-password/PermitRootLogin yes/' /etc/ssh/sshd_config
+systemctl enable ssh
+systemctl restart ssh
