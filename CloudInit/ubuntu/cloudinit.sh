41b980da1fb41f800f1cfb655b0f97d10123efbc
Chaboudez Lucien <lucien.chaboudez@epfl.ch>
2022-07-07 14:06:52 +0200

    NonInteractive install

--- CloudInit/ubuntu/cloudinit.sh
+++ CloudInit/ubuntu/cloudinit.sh
@@ -2,10 +2,11 @@
 
 # Mise à jour des packages
 apt-get update
-apt-get -y upgrade
+# Pour ne pas avoir les demandes interactives pour l'install (avec entrées utilisateurs)
+export DEBIAN_FRONTEND=noninteractive
+apt-get -yq upgrade
 
 # Activation du SSH
 echo "Activating SSH..."
-#sed -i 's/#PermitRootLogin prohibit-password/PermitRootLogin yes/' /etc/ssh/sshd_config
 systemctl enable ssh
-systemctl restart ssh
+systemctl start ssh
